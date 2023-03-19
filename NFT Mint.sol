7de5b2b86d643f8831ed76606044d6151bb9252d
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2023-03-19 17:55:49 +0300

    Update NFT Mint.sol
    Upgrade to the ERC-1155 standard from the ERC-721 standard

--- NFT Mint.sol
+++ NFT Mint.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-// Import the OZ contracts
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// GameItem follows ERC721 contract from openzeppelin
-contract GameItem is ERC721 {
-
-    constructor() ERC721("GameItem", "ITM") {
-        // mint an NFT to yourself
-        _mint(msg.sender, 1);
+contract GameAsset is ERC1155, Ownable {
+    mapping (uint256 => address) private _owners;
+    
+    constructor(string memory uri) ERC1155(uri) {
+    }
+    
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        return _owners[tokenId];
+    }
+    
+    function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public override {
+        require(_owners[id] == from, "ERC1155: caller is not the owner");
+        _safeTransferFrom(from, to, id, amount, data);
+        _owners[id] = to;
+    }
+    
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        _mint(to, id, amount, data);
+        _owners[id] = to;
     }
 }
+
+
+
